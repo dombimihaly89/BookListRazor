02836a1b130372f7210da30c26759d2400ae123d
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-16 16:11:35 +0200

    Added the Book class as a model to the project with three properties

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        // It is going to be a unique key so we can add this 'key' data annotation.
+        // It is going to generate automatically an ID.
+        [Key]
+        public int Id { get; set; }
+
+        // Required means that the annotated property cannot be null.
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
